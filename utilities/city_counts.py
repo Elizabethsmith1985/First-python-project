cd67c9aea9ef4d9c2d9b8834eaece7e798194439
Elizabethsmith1985 <Elizabethsmith2686@yahoo.com>
2025-11-08 18:41:43 +0000

    counting cities

---
+++ utilities/city_counts.py
@@ -0,0 +1,18 @@
+import csv
+def count_cities(input_file):
+    with open(input_file, newline='') as f:
+       reader = csv.reader(f)
+       next(reader) # skip header row
+       city_counts = {}
+       for row in reader:
+           city = row[2].strip()
+           if city in city_counts:
+               city_counts[city] += 1
+           else:
+               city_counts[city] += 1
+    return city_counts
+if __name__ == "__main__":
+    input_name = input("Input csv filename: ").strip()
+    results = count_cities(input_name)
+    for city, count in results.items():
+        print(city, ":", count)
